7b201e539cad99c8c0d0273f30f96dae322bc6c3
dimarudenk <dimarudenk@gmail.com>
2024-01-11 02:43:29 +0300

    Добавьте файлы проекта.

---
+++ Гонка2/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Гонка2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 615;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(615, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать картинку";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 427);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Счет: 0";
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button2.Location = new System.Drawing.Point(0, 414);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить точку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Гонка2/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Гонка2
+{
+    public partial class Form1 : Form
+    {
+        private const int gridSize = 40;
+        private List<Point> points = new List<Point>();
+        private Point movingPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += PictureBox1_Paint;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисуем сетку
+            using (Pen gridPen = new Pen(Color.LightGray))
+            {
+                for (int x = 0; x < pictureBox1.Width; x += gridSize)
+                {
+                    e.Graphics.DrawLine(gridPen, x, 0, x, pictureBox1.Height);
+                }
+                for (int y = 0; y < pictureBox1.Height; y += gridSize)
+                {
+                    e.Graphics.DrawLine(gridPen, 0, y, pictureBox1.Width, y);
+                }
+            }
+
+            // Рисуем точки
+            foreach (Point point in points)
+            {
+                e.Graphics.FillEllipse(Brushes.Black, point.X - 4, point.Y - 4, 8, 8);
+                this.Text = point.X + " " + point.Y;
+            }
+
+            using (Pen gridPen = new Pen(Color.Red))
+                for (int x = 0; x < points.Count - 1; x++)
+                {
+                    Point one = points[x];
+                    Point two = points[x + 1];
+
+                    e.Graphics.DrawLine(gridPen, one.X, one.Y, two.X, two.Y);
+                }
+        
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            
+            if (e.Button == MouseButtons.Right && movingPoint.IsEmpty)
+            {
+                int newX = (e.X / gridSize) * gridSize;
+                int newY = (e.Y / gridSize) * gridSize;
+
+                movingPoint = new Point(newX, newY);
+                this.Text = newX + " " + newY;
+            }
+
+            
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right && !movingPoint.IsEmpty)
+            {
+                int newX = (e.X / gridSize) * gridSize;
+                int newY = (e.Y / gridSize) * gridSize;
+
+
+                for (int i = 0; i < points.Count; i++)
+                {
+                    Point p = points[i];
+                    if(p.X == movingPoint.X && p.Y == movingPoint.Y)
+                    {
+                        points[i] = new Point(newX,newY);
+
+                    }
+
+                }
+                
+
+                this.Text = newX + " " + newY;
+                pictureBox1.Invalidate(); // Перерисовываем PictureBox
+                movingPoint = Point.Empty;
+
+            }
+
+            // Начинаем перемещение точки
+            //movingPoint = Point.Empty;
+            //foreach (Point point in points)
+            //{
+            //    if (Math.Abs(point.X - e.X) <= 5 && Math.Abs(point.Y - e.Y) <= 5)
+            //    {
+            //        movingPoint = point;
+            //        break;
+            //    }
+            //}
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (e.Button == MouseButtons.Right && !movingPoint.IsEmpty)
+            {
+                int newX = (e.X / gridSize) * gridSize;
+                int newY = (e.Y / gridSize) * gridSize;
+                this.Text = newX+ " " + newY;
+                
+
+            }
+
+            // Перемещаем точку
+            //if (e.Button == MouseButtons.Left && !movingPoint.IsEmpty)
+            //{
+            //    movingPoint.X = (e.X / gridSize) * gridSize;
+            //    movingPoint.Y = (e.Y / gridSize) * gridSize;
+            //    pictureBox1.Invalidate(); // Перерисовываем PictureBox
+            //}
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Добавляем новую точку на пересечение сетки
+                int newX = (e.X / gridSize) * gridSize;
+                int newY = (e.Y / gridSize) * gridSize;
+                points.Add(new Point(newX, newY));
+                listBox1.Items.Add(String.Format("{0}: x={1}, y={2}", points.Count, newX / gridSize, newY / gridSize));
+                pictureBox1.Invalidate(); // Перерисовываем PictureBox
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.RemoveAt(listBox1.SelectedIndex);
+
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            pictureBox1.Invalidate(); // Перерисовываем PictureBox
+
+        }
+    }
+}
